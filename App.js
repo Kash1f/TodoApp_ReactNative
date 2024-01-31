8ba7c664222dd1f35a018aa84a0a058f1a3475b4
Kashif <ahmed.kashif1001@gmail.com>
2024-01-31 17:18:23 +0500

    list of goals

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View>
       <View>
-      <TextInput placeholder='Your course goal!'/>
-       </View>
-      <View></View>
-
-    
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
